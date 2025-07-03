a5d3bff7e90631b204ee06fea3b274c22fae9913
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-07-03 22:28:23 +0300

    add departmentdeleteview class to init

--- employee_MS/employee_app/views/__init__.py
+++ employee_MS/employee_app/views/__init__.py
@@ -1,4 +1,4 @@
-from .department_views import DepartmentCreateView
+from .department_views import DepartmentCreateView , DepartmentDeleteView
 from .employee_views import EmployeeListCreateView
 
-__all__ = ['EmployeeListCreateView', ' DepartmentCreateView']
+__all__ = ['EmployeeListCreateView', 'DepartmentCreateView', 'DepartmentDeleteView']
