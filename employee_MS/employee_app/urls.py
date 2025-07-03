868c47b7ff9b0072696e50582c184e0b3e92cff4
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-07-04 00:13:21 +0300

    add hr  login and register urls

--- employee_MS/employee_app/urls.py
+++ employee_MS/employee_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import EmployeeListCreateView , DepartmentCreateView , DepartmentDeleteView ,DepartmentUpdateView
+from .views import EmployeeListCreateView , DepartmentCreateView , DepartmentDeleteView ,DepartmentUpdateView , HRRegisterView , HRLoginView
 
 urlpatterns = [
     path('employees/', EmployeeListCreateView.as_view(), name='employee-list-create'),
     path('departments/', DepartmentCreateView.as_view(), name='department-create'), 
     path('departments/<int:pk>/', DepartmentDeleteView.as_view(), name='department-delete'),
     path('departments/<int:pk>/update/', DepartmentUpdateView.as_view(), name='department-update'),
+    path('hr-login/', HRLoginView.as_view(), name='hr-login'),
+    path('hr-register/', HRRegisterView.as_view(), name='hr-register'),
 ]
