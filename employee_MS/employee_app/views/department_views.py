5b48b6e2bd60dec6252686b1855a46cda08ed149
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-07-04 00:16:11 +0300

    add authentication to departmant views

--- employee_MS/employee_app/views/department_views.py
+++ employee_MS/employee_app/views/department_views.py
@@ -1,16 +1,20 @@
 from rest_framework import generics
 from ..models import Department
 from ..serializers import DepartmentSerializer
+from ..permissions import IsHR
+from rest_framework.permissions import IsAuthenticated
 
 class DepartmentCreateView(generics.ListCreateAPIView):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
+    permission_classes = [IsAuthenticated, IsHR]
 
 class DepartmentDeleteView(generics.DestroyAPIView):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer  
-
+    permission_classes = [IsAuthenticated, IsHR]
+    
 class DepartmentUpdateView(generics.UpdateAPIView):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
-    
+    permission_classes = [IsAuthenticated, IsHR]
