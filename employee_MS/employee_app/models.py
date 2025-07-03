3e9dc37c65225b5f300854b9db6895be53083a84
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-07-03 14:53:24 +0300

    create department model and referece employee.department

--- employee_MS/employee_app/models.py
+++ employee_MS/employee_app/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
 
 class Employee(models.Model):
     first_name = models.CharField(max_length=100)
@@ -9,5 +13,7 @@ class Employee(models.Model):
     email = models.CharField(max_length=255) 
     password = models.CharField(max_length=255) 
     position = models.CharField(max_length=200) 
-    department = models.ForeignKey()
+    department = models.ForeignKey(
+        Department, on_delete=models.CASCADE, related_name="employees"
+    )
     hire_date = models.DateField()
