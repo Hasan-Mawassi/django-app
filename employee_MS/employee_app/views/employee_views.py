af0fe19960407e6dd53a677a1a882071cdddad3f
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-07-04 00:15:08 +0300

    add authentication to employees

--- employee_MS/employee_app/views/employee_views.py
+++ employee_MS/employee_app/views/employee_views.py
@@ -1,7 +1,11 @@
 from rest_framework import generics
 from ..models import Employee
 from ..serializers import EmployeeSerializer
+from ..permissions import IsHR
+from rest_framework.permissions import IsAuthenticated
+
 
 class EmployeeListCreateView(generics.ListCreateAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+    permission_classes = [IsAuthenticated, IsHR]
