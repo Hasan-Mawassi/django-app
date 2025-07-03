c871f748c2b429ec715f91507d049bdf56730801
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-07-03 22:26:30 +0300

    add department delete class

--- employee_MS/employee_app/views/department_views.py
+++ employee_MS/employee_app/views/department_views.py
@@ -5,3 +5,7 @@ from ..serializers import DepartmentSerializer
 class DepartmentCreateView(generics.ListCreateAPIView):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
+
+class DepartmentDeleteView(generics.DestroyAPIView):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer  
