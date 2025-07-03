0e36ae30d58abe4806e1dc15bef789d2ded9b7d4
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-07-03 14:40:57 +0300

    employee model

--- employee_MS/employee_app/models.py
+++ employee_MS/employee_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=255) 
+    password = models.CharField(max_length=255) 
+    position = models.CharField(max_length=200) 
+    department = models.ForeignKey()
+    hire_date = models.DateField()
