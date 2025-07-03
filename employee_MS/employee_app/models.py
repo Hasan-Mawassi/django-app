902731771ee72bddc2bd5048d078124c2692e60f
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-07-03 15:34:06 +0300

    add str func to the models

--- employee_MS/employee_app/models.py
+++ employee_MS/employee_app/models.py
@@ -18,6 +18,8 @@ class Employee(models.Model):
     )
     hire_date = models.DateField()
 
+    def __str__(self):
+         return f"{self.first_name} {self.last_name} - {self.position}"
 
 class WorkSchedule(models.Model):
     start_time = models.DateField()
@@ -27,3 +29,5 @@ class WorkSchedule(models.Model):
         Employee, on_delete=models.CASCADE ,related_name="work_schedules"
     )
     
+    def __str__(self):
+        return f"{self.work_day} - {self.employee.first_name}"
