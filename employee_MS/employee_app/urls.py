a919262f562aefa3be2f309f08e39e4e0a7c586e
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-07-03 22:42:26 +0300

    update the update url

--- employee_MS/employee_app/urls.py
+++ employee_MS/employee_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import EmployeeListCreateView , DepartmentCreateView , DepartmentDeleteView 
+from .views import EmployeeListCreateView , DepartmentCreateView , DepartmentDeleteView ,DepartmentUpdateView
 
 urlpatterns = [
     path('employees/', EmployeeListCreateView.as_view(), name='employee-list-create'),
     path('departments/', DepartmentCreateView.as_view(), name='department-create'), 
     path('departments/<int:pk>/', DepartmentDeleteView.as_view(), name='department-delete'),
-    path('departments/<int:pk>/', DepartmentDeleteView.as_view(), name='department-delete'),
+    path('departments/<int:pk>/update/', DepartmentUpdateView.as_view(), name='department-update'),
 ]
