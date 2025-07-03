99116c516a811d28d3bd5632d68cbf60984ea94b
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-07-03 22:29:04 +0300

    add delete path to url"

--- employee_MS/employee_app/urls.py
+++ employee_MS/employee_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import EmployeeListCreateView , DepartmentCreateView
+from .views import EmployeeListCreateView , DepartmentCreateView , DepartmentDeleteView 
 
 urlpatterns = [
     path('employees/', EmployeeListCreateView.as_view(), name='employee-list-create'),
-    path('departments/', DepartmentCreateView.as_view(), name='department-create'),
+    path('departments/', DepartmentCreateView.as_view(), name='department-create'), 
+    path('departments/<int:pk>/', DepartmentDeleteView.as_view(), name='department-delete'),
 ]
