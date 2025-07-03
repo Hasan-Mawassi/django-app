d1ba397cceaf3f5341b597ed25d379987342451c
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-07-03 17:20:03 +0300

    add departments url

--- employee_MS/employee_app/urls.py
+++ employee_MS/employee_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import EmployeeListCreateView
+from .views import EmployeeListCreateView , DepartmentCreateView
 
 urlpatterns = [
     path('employees/', EmployeeListCreateView.as_view(), name='employee-list-create'),
+    path('departments/', DepartmentCreateView.as_view(), name='department-create'),
 ]
