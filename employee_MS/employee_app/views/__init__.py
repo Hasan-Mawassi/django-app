4e52a1137c0765767293c480368a89177a164fe4
Hasan-Mawassi <hasanmawassi0@gmail.com>
2025-07-04 12:28:40 +0300

    add class to init

--- employee_MS/employee_app/views/__init__.py
+++ employee_MS/employee_app/views/__init__.py
@@ -1,9 +1,10 @@
 from .department_views import DepartmentCreateView , DepartmentDeleteView , DepartmentUpdateView
-from .employee_views import EmployeeListView , EmployeeCreateView
+from .employee_views import EmployeeListView , EmployeeCreateView , EmployeeUpdateView
 from .hr_views import HRRegisterView , HRLoginView
 __all__ = [
     'EmployeeListView', 
     'EmployeeCreateView',
+    'EmployeeUpdateView',
     'DepartmentCreateView', 
     'DepartmentDeleteView', 
     'DepartmentUpdateView',
